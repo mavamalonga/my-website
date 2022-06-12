6390ae14f2f4431cf032b268e544beb53302c211
Alpha <mavamalonga.alpha@gmail.com>
2022-06-12 16:42:36 +0200

    addBlogsPage

--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'app/index.html')
 
-def blog(request):
-    return render(request, 'app/blog.html')
+def blogs(request):
+    return render(request, 'app/blogs.html')
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import index
+from app.views import index, blogs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('blogs/', blogs, name='blogs'),
 ]
