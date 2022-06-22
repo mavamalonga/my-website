b0c94edd9c45753a62a9917c6f9520a65de494c9
Alpha <mavamalonga.alpha@gmail.com>
2022-06-22 14:52:55 +0200

    deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['alpha-mavamalonga.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['alpha-mavamalonga.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
