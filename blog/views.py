926abb1448b2a73107a44a57bf9df8319f246cd8
Alpha <mavamalonga.alpha@gmail.com>
2022-07-04 20:35:24 +0200

    jinjaAllReals

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,16 @@
+from multiprocessing import context
 from django.shortcuts import get_object_or_404, render
-#from blog.models import Article 
+from APIREST.models import Project, Skill, Badge
 
 
 def blogs(request):
-    """
-    articles = Article.objects.all()
-    articles = sorted(articles, key=lambda k:k.date_published, reverse=True)
-    context = {'articles': articles}
-    """
-    return render(request, 'blog/blogs.html')
+    projects = []
+    for project in  Project.objects.all():
+        badges = Badge.objects.filter(projects__id=project.id)
+        skills = Skill.objects.filter(projects__id=project.id)
+        projects.append({'project':project, 'badges':badges, 'skills':skills})
+    context = {'projects': projects}
+    return render(request, 'blog/blogs.html', context)
 
 def blog(request, blog_id):
     #article = get_object_or_404(Article, pk=blog_id)
