c4c0d466472aee307fb1ab47608f7d7c25fc4a93
Alpha <mavamalonga.alpha@gmail.com>
2022-07-05 21:19:21 +0200

    initFilterReals

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from multiprocessing import context
+import django
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from APIREST.models import Project, Skill, Badge
@@ -6,6 +7,64 @@ import math
 
 
 def blogs(request):
+    if request.GET.get('filter') == 'python':
+        projects = []
+        only_projects = []
+        for project in  Project.objects.all():
+            badges = Badge.objects.filter(projects__id=project.id)
+            python_badges = Badge.objects.filter(name__in=['Python', 'Django', 'Flask'])
+
+            if python_badges[0] in badges:
+                skills = Skill.objects.filter(projects__id=project.id)
+                projects.append({'project':project, 'badges':badges, 'skills':skills})
+                only_projects.append(project)
+                continue
+            elif python_badges[1] in badges:
+                skills = Skill.objects.filter(projects__id=project.id)
+                projects.append({'project':project, 'badges':badges, 'skills':skills})
+                only_projects.append(project)
+                continue
+            elif python_badges[2] in badges:
+                skills = Skill.objects.filter(projects__id=project.id)
+                projects.append({'project':project, 'badges':badges, 'skills':skills})
+                only_projects.append(project)
+                continue
+        
+        # Set up Paginator
+        p = Paginator(only_projects, 9)
+        page = request.GET.get('page')
+        p_projects = p.get_page(page)
+        nums_pages = "a" * p_projects.paginator.num_pages
+
+        context = {'projects': projects, 'p_projects': p_projects, 'nums_pages': nums_pages}
+        return render(request, 'blog/blogs.html', context)
+
+
+    if request.GET.get('filter') == 'django':
+        projects = []
+        only_projects = []
+        for project in  Project.objects.all():
+            badges = Badge.objects.filter(projects__id=project.id)
+            django_badge = Badge.objects.get(name='Django')
+
+            if django_badge in badges:
+                skills = Skill.objects.filter(projects__id=project.id)
+                projects.append({'project':project, 'badges':badges, 'skills':skills})
+                only_projects.append(project)
+                continue
+        
+        # Set up Paginator
+        p = Paginator(only_projects, 9)
+        page = request.GET.get('page')
+        p_projects = p.get_page(page)
+        nums_pages = "a" * p_projects.paginator.num_pages
+
+        context = {'projects': projects, 'p_projects': p_projects, 'nums_pages': nums_pages}
+        return render(request, 'blog/blogs.html', context)
+
+
+
+
     # Set up Paginator
     p = Paginator(Project.objects.all(), 9)
     page = request.GET.get('page')
@@ -17,7 +76,6 @@ def blogs(request):
         badges = Badge.objects.filter(projects__id=project.id)
         skills = Skill.objects.filter(projects__id=project.id)
         projects.append({'project':project, 'badges':badges, 'skills':skills})
-    
     context = {'projects': projects, 'p_projects': p_projects, 'nums_pages': nums_pages}
     return render(request, 'blog/blogs.html', context)
 
