6006ce67a0a296110bd1f81e748650dd04bff307
Alpha <mavamalonga.alpha@gmail.com>
2022-07-05 16:19:18 +0200

    jinjaPagination

--- blog/views.py
+++ blog/views.py
@@ -5,17 +5,17 @@ from APIREST.models import Project, Skill, Badge
 
 
 def blogs(request):
-    projects = []
-    for project in  Project.objects.all():
-        badges = Badge.objects.filter(projects__id=project.id)
-        skills = Skill.objects.filter(projects__id=project.id)
-        projects.append({'project':project, 'badges':badges, 'skills':skills})
-    
     # Set up Paginator
     p = Paginator(Project.objects.all(), 9)
     page = request.GET.get('page')
     p_projects = p.get_page(page)
 
+    projects = []
+    for project in  p_projects:
+        badges = Badge.objects.filter(projects__id=project.id)
+        skills = Skill.objects.filter(projects__id=project.id)
+        projects.append({'project':project, 'badges':badges, 'skills':skills})
+    
     context = {'projects': projects, 'p_projects': p_projects}
     return render(request, 'blog/blogs.html', context)
 
