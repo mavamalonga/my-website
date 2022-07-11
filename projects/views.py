c9abef98e1b5049c5f59806157a10ef445a23dfa
Alpha <mavamalonga.alpha@gmail.com>
2022-07-11 16:03:17 +0200

    upgradeNavBar

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from APIREST.models import Project, Skill, Badge
@@ -5,43 +6,27 @@ from APIREST.models import Project, Skill, Badge
 
 def projects(request):
 
-    technos = ['Python', 'Django', 'Flask', 'JavaScript', 'React', 'Node', 'Rust']
+    # My technological expertise
+    techs = ['Python', 'Django', 'Flask', 'JavaScript', 'React', 'Node', 'Rust']
+    
+    queryset = Project.objects.all()
 
-    if request.GET.get('filter') in technos:
-        projects = []
-        only_projects = []
-        for project in  Project.objects.all():
-            badges = Badge.objects.filter(projects__id=project.id)
-            filter_badge = get_object_or_404(Badge, name=request.GET.get('filter'))
-
-            if filter_badge in badges:
-                skills = Skill.objects.filter(projects__id=project.id)
-                projects.append({'project':project, 'badges':badges, 'skills':skills})
-                only_projects.append(project)
-                continue
-
-        
-        # Set up Paginator
-        p = Paginator(only_projects, 6)
-        page = request.GET.get('page')
-        p_projects = p.get_page(page)
-        nums_pages = "a" * p_projects.paginator.num_pages
-
-        context = {'projects': projects, 'p_projects': p_projects, 'nums_pages': nums_pages}
-        return render(request, 'projects/projects.html', context)
-
+    # selects the projects of a technology
+    if request.GET.get('filter') in techs:
+        queryset = [project for project in Project.objects.all()
+            if get_object_or_404(Badge, name=request.GET.get('filter')) in 
+            Badge.objects.filter(projects__id=project.id)]
 
     # Set up Paginator
-    p = Paginator(Project.objects.all(), 6)
+    paginator = Paginator(queryset, 6)
     page = request.GET.get('page')
-    p_projects = p.get_page(page)
-    nums_pages = "a" * p_projects.paginator.num_pages
+    projects_of_page = paginator.get_page(page)
+    number_of_pages = "a" * projects_of_page.paginator.num_pages
 
-    projects = []
-    for project in  p_projects:
-        badges = Badge.objects.filter(projects__id=project.id)
-        skills = Skill.objects.filter(projects__id=project.id)
-        projects.append({'project':project, 'badges':badges, 'skills':skills})
-    context = {'projects': projects, 'p_projects': p_projects, 'nums_pages': nums_pages}
+    # filter badges and project tasks
+    projects = [
+        {'project':project, 'badges':Badge.objects.filter(projects__id=project.id), 
+        'skills':Skill.objects.filter(projects__id=project.id)} for project in  projects_of_page]
+  
+    context = {'projects': projects, 'projects_of_page': projects_of_page, 'number_of_pages': number_of_pages}
     return render(request, 'projects/projects.html', context)
-
