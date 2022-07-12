5a7e376593756499ff21902c74e3c71e34568295
Alpha <mavamalonga.alpha@gmail.com>
2022-07-12 17:11:08 +0200

    SetUpPipelineCICD

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -2,6 +2,24 @@ import os
 import sys
 from pathlib import Path
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_SDK'),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 load_dotenv()  # take environment variables from .env.
 
@@ -24,6 +42,7 @@ AUTH_USER_MODEL = 'APIREST.User'
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
