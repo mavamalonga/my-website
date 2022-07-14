dfd4d3d4449757b3bd80fbd0e3f7dfbfdb8e3eb9
Alpha <mavamalonga.alpha@gmail.com>
2022-07-14 19:56:08 +0200

    fake8

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,6 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 def get_debug():
     if os.getenv("DEBUG") is not None:
