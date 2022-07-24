e3885fcebdd356c25477023e982217c21ad04221
Alpha <mavamalonga.alpha@gmail.com>
2022-07-24 22:52:30 +0200

    DOMbadges

--- dashboard/static/js/dashboard/api_request.js
+++ dashboard/static/js/dashboard/api_request.js
@@ -14,9 +14,13 @@ function get_projects(path){
     const work_card = document.querySelectorAll(".work__card");
 
     fetch_api(url).then(function(result) {
-        for (let i = 0; i < result.length; i++){
+        for (let i = 0; i < 6; i++){
             work_card[i].querySelector('.work__img').setAttribute('src', result[i]['image']);
             work_card[i].querySelector('.work__title').innerHTML = result[i]['title'];
+            for (let x = 0; x < result[i]['badges'].length; x++){
+                console.log(result[i]['badges'][x]['name']);
+                work_card[i].querySelectorAll('.badge.badge-info')[x].innerHTML = result[i]['badges'][x]['name'];
+            }
         }
     })
 }
